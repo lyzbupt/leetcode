7a3dd1734148faea61f0ccbccc13b19d32cef93b
lyz <812831228@qq.com>
2018-08-01 23:51:11 +0800

    E30

---
+++ 337.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+/*
+class Solution {
+    public int rob(TreeNode root) {
+    	if (root == null) return 0;
+        int res = 0;
+        if(root.left != null){
+        	res += rob(root.left.left)+rob(root.left.right);
+        }
+        if(root.right != null){
+        	res+= rob(root.right.left)+rob(root.right.right);
+        }
+        return Math.max(root.val + res, rob(root.left)+rob(root.right));
+    }
+}
+*/
+
+class Solution {
+	public int rob(TreeNode root) {
+	    int[] res = robSub(root);
+	    return Math.max(res[0], res[1]);
+	}
+
+	private int[] robSub(TreeNode root) {
+	    if (root == null) return new int[2];
+	    
+	    int[] left = robSub(root.left);
+	    int[] right = robSub(root.right);
+	    int[] res = new int[2];
+
+	    res[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+	    res[1] = root.val + left[0] + right[0];
+	    
+	    return res;
+	}
+}
+
