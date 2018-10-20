b3caa9f1dcf8a462e0b346e2167ca18c587699f5
lyz <812831228@qq.com>
2018-10-19 21:54:29 -0700

    Otc19

--- 116.java
+++ 116.java
@@ -8,6 +8,17 @@
  */
 public class Solution {
     public void connect(TreeLinkNode root) {
-        
-    }
+        if(root == null) return;
+        TreeLinkNode mostLeft = root;
+        TreeLinkNode cur = null;
+        while(mostLeft.left!=null){
+        	cur = mostLeft;
+        	while(cur!=null){
+        		cur.left.next = cur.right;
+        		if(cur.next != null) cur.right.next = cur.next.left;
+        		cur = cur.next;
+        	}
+        	mostLeft = mostLeft.left;
+        }
+    } 
 }
